1c62d60e5936d7ce6f8d2b5c29ece9901861eb22
Hadiaelkadaoui <hadiaelkadaoui@gmail.com>
2023-05-30 17:56:35 -0700

    FLAGS

--- get_flag.c
+++ get_flag.c
@@ -1,35 +1,29 @@
 #include "main.h"
-
 /**
- * get_flag - Finds active flags
- * @format: Format of the string
- * @i: parameter
- * Return: All active Flags
- */
-
-int get_flag(const char *format, int *i)
+* get_flags - Calculates active flags
+* @format: Formatted string in which to print the arguments
+* @i: take a parameter.
+* Return: Flags:
+*/
+int get_flags(const char *format, int *i)
 {
-	int j, curr_i;
-	int flag = 0;
-
-	const char FLAGSIZE_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGSIZE_ARR[] = {FLAG_MINUS, FLAG_PLUS,
-		FLAG_ZERO, FLAG_HASH, FLAG_SPACE, 0};
-
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		for (j = 0; FLAGSIZE_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGSIZE_CH[j])
-			{
-				flag |= FLAGSIZE_ARR[j];
-				break;
-			}
-
-		if (FLAGSIZE_CH[j] == 0)
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (flag);
+/* - + 0 # ' ' */
+/* 1 2 4 8  16 */
+int j, curr_i;
+int flags = 0;
+const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+{
+for (j = 0; FLAGS_CH[j] != '\0'; j++)
+if (format[curr_i] == FLAGS_CH[j])
+{
+flags |= FLAGS_ARR[j];
+break;
+}
+if (FLAGS_CH[j] == 0)
+break;
+}
+*i = curr_i - 1;
+return (flags);
 }
